f1640e04389bc29048b6b8ef5eafe3d2f1766d7f
zaheenrakib <rakibislambest@gmail.com>
2024-02-18 13:13:17 +0600

    Fix Some Bug

--- script/index.js
+++ script/index.js
@@ -1,5 +1,16 @@
 const buyTickects = document.getElementById('buy-tickets');
 
-buyTickects.addEventListener('click', function(){
+buyTickects.addEventListener('click', function () {
     location.href = "#ph-paribahan";
 });
+
+
+// const selectTickets = document.getElementById('A1');
+
+// selectTickets.addEventListener('click',function(){
+//     console.log(selectTickets.classList);
+//    selectTickets.classList.add('bg-green-500');
+//    element.classList.add('bg-green-400')
+// })
+
+
