358bbf565486d963969c8c0db7546201dc333a07
Zaheen Rakib <rakibzaheen@gmail.com>
2024-02-19 14:42:27 +0600

    Some bug Fix

--- script/index.js
+++ script/index.js
@@ -5,6 +5,8 @@ buyTickects.addEventListener('click', function () {
     location.href = "#ph-paribahan";
 });
 
+// Hide Elements
+hideElementById('coupon-container');
 
 // Select Seat
 
@@ -15,56 +17,55 @@ let totalSeat = 40;
 for (const seat of allSeat){
     seat.addEventListener('click',function(e){
 
-        const curentValue = seat.innerHTML;
+        if(count < 4){
+            const curentValue = seat.innerHTML;
         
-        const fixedPrice = document.getElementById('ticket-price').innerText;
-
-        const selectContainer = document.getElementById('append-seat');
-
-        const creteDiv = document.createElement('div');
-        creteDiv.classList.add('flex','justify-between');
-
-
-        const titleSeatName = document.createElement("h2");
-        titleSeatName.innerText = curentValue;
-        titleSeatName.classList.add('card-title')
-        selectContainer.appendChild(titleSeatName);
-
-        const seatClassName = document.createElement('h2');
-        seatClassName.innerText = "Economoy";
-        seatClassName.classList.add('card-title');
-        selectContainer.appendChild(seatClassName);
-
-        const seatCurentPrice = document.createElement('h2');
-        seatCurentPrice.innerText = fixedPrice;
-        seatCurentPrice.classList.add('card-title');
-        selectContainer.appendChild(seatCurentPrice);
-
-        creteDiv.appendChild(titleSeatName);
-        creteDiv.appendChild(seatClassName);
-        creteDiv.appendChild(seatCurentPrice);
-        selectContainer.appendChild(creteDiv);
-        
-        seat.classList.add('btn-disableded');
-        seat.classList.add('text-white');
-
-        totalSeat = totalSeat - 1;
-        setInnerText('total-seat',totalSeat);
-
-        count = count + 1;
-        setInnerText('seat-count', count);
-
-
-        totalCost('total-prices', fixedPrice);
-
-        gradTotalCost('grad-total' , fixedPrice);
-
-
-        if(count >= 4){
-            Swal.fire("You Select Max Number");
-            count = count - 1;   
-            totalSeat = totalSeat + 1;
+            const fixedPrice = document.getElementById('ticket-price').innerText;
+    
+            const selectContainer = document.getElementById('append-seat');
+    
+            const creteDiv = document.createElement('div');
+            creteDiv.classList.add('flex','justify-between');
+    
+    
+            const titleSeatName = document.createElement("h2");
+            titleSeatName.innerText = curentValue;
+            titleSeatName.classList.add('card-title')
+            selectContainer.appendChild(titleSeatName);
+    
+            const seatClassName = document.createElement('h2');
+            seatClassName.innerText = "Economoy";
+            seatClassName.classList.add('card-title');
+            selectContainer.appendChild(seatClassName);
+    
+            const seatCurentPrice = document.createElement('h2');
+            seatCurentPrice.innerText = fixedPrice;
+            seatCurentPrice.classList.add('card-title');
+            selectContainer.appendChild(seatCurentPrice);
+    
+            creteDiv.appendChild(titleSeatName);
+            creteDiv.appendChild(seatClassName);
+            creteDiv.appendChild(seatCurentPrice);
+            selectContainer.appendChild(creteDiv);
+            
+            seat.classList.add('btn-disableded');
+            seat.classList.add('text-white');
+    
+            totalSeat = totalSeat - 1;
+            setInnerText('total-seat',totalSeat);
+    
+            count = count + 1;
+            setInnerText('seat-count', count);
+    
+    
+            totalCost('total-prices', fixedPrice);
+    
+            gradTotalCost('grad-total' , fixedPrice);
         }
+        else{
+            Swal.fire("You Select Max Number Seat");
+        }
+
     })
 }
 
@@ -72,7 +73,7 @@ for (const seat of allSeat){
 const cuponCode = document.getElementById('coupon-btn');
 let couponValue = document.getElementById("coupon-value");
 cuponCode.addEventListener('click',function(){
-
+    
 })
 
 
